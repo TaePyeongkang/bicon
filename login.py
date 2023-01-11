2054247c0c01273942f1073edc2a46a9958bbb0b
TaePyeongkang <dramayeong@naver.com>
2023-01-11 21:06:29 +0900

    2aㄴ

--- login.py
+++ login.py
@@ -13,6 +13,7 @@ class Login(QWidget, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.stackedWidget.setCurrentIndex(1)
         self.stackedWidget_3.setCurrentIndex(0)
         self.login.clicked.connect(self.MoveLoginPage)
         self.home1.clicked.connect(self.MainPage)
@@ -37,7 +38,7 @@ class Login(QWidget, form_class):
                                    charset='utf8')
         cursor = conn.cursor()
         cursor.execute(
-            f"INSERT INTO calendar (번호,이름,내용,날짜) VALUES ('{(self.result[0][0])}','{self.result[0][1]}','{self.line}','{self.date}')")
+            f"INSERT INTO calendar (구분,번호,이름,내용,날짜) VALUES ('{(self.result[0][0])}','{(self.result[0][1])}','{self.result[0][2]}','{self.line}','{self.date}')")
         cursor.execute(f"SELECT * FROM calendar where 날짜 = '{self.date}'")
         self.a = cursor.fetchall()
         conn.commit()
@@ -69,10 +70,10 @@ class Login(QWidget, form_class):
     def Enter_check(self):                          # 입실 눌렀을때 함수
         if self.logyn == True:                      # 로그인 여부가 참일떄
             self.stackedWidget.setCurrentIndex(2)   # 입실 버튼누를수 있는 페이지로 이동
-            self.enter_time.setText(f"{self.result[0][6]}") # 입실 누르면 데이터에 저장된 시간 라벨에 표시
-            self.finish_time.setText(f"{self.result[0][7]}")    # 퇴실
-            self.out_time.setText(f"{self.result[0][10]}")      # 외출
-            self.back_time.setText(f"{self.result[0][11]}")     # 복귀
+            self.enter_time.setText(f"{self.result[0][7]}") # 입실 누르면 데이터에 저장된 시간 라벨에 표시
+            self.finish_time.setText(f"{self.result[0][8]}")    # 퇴실
+            self.out_time.setText(f"{self.result[0][11]}")      # 외출
+            self.back_time.setText(f"{self.result[0][12]}")     # 복귀
         else:                                                   # 로그인 여부가 거짓일때
             QMessageBox.information(self, "로그인", f"로그인 해야 출석체크 할수있습니다.") # 메세지 나오게 함
 
@@ -103,14 +104,20 @@ class Login(QWidget, form_class):
         elif self.result == ():                                         # 일치하는 값이 없으면 ()로 나옴 그때 오류 표시
             QMessageBox.critical(self, "로그인 오류", "일치하는 정보가 없습니다")
 
-        elif self.result[0][2] == self.id and self.result[0][3] == self.pw:     # 아이디와 비밀번호가 일치했을때  로그인 성공 메시지 표시함
-            QMessageBox.information(self, "로그인 성공", f"{self.result[0][1]}님 로그인 성공하셨습니다")
-            self.stackedWidget_3.setCurrentIndex(1)                     # 로그인 버튼이 로그아웃 버튼으로 변경
-            self.stackedWidget.setCurrentIndex(1)                       # 로그인시 메인페이지로 이동
-            self.logyn = True                                           # 로그인 여부가 참
+        elif self.result[0][3] == self.id and self.result[0][4] == self.pw:     # 아이디와 비밀번호가 일치했을때  로그인 성공 메시지 표시함
+            if self.result[0][1] <= 27:
+                QMessageBox.information(self, "로그인 성공", f"{self.result[0][2]}님 로그인 성공하셨습니다")
+                self.stackedWidget_3.setCurrentIndex(1)                     # 로그인 버튼이 로그아웃 버튼으로 변경
+                self.stackedWidget.setCurrentIndex(1)                       # 로그인시 메인페이지로 이동
+                self.logyn = True                                           # 로그인 여부가 참
+            else:
+                QMessageBox.information(self, "로그인 성공", f"{self.result[0][2]} 교수님 로그인 성공하셨습니다")
+                self.stackedWidget_3.setCurrentIndex(1)                     # 로그인 버튼이 로그아웃 버튼으로 변경
+                self.stackedWidget.setCurrentIndex(1)                       # 로그인시 메인페이지로 이동
+                self.logyn = True                                           # 로그인 여부가 참
 
     def Logout(self):                                                   # 로그아웃
-        QMessageBox.information(self, "로그아웃 성공", f"{self.result[0][1]}님 로그아웃하셨습니다")
+        QMessageBox.information(self, "로그아웃 성공", f"{self.result[0][2]}님 로그아웃하셨습니다")
         self.stackedWidget_3.setCurrentIndex(0)                         # 로그아웃 버튼이 로그인으로 변경
         # self.stackedWidget_4.setCurrentIndex(1)
         self.logyn = False                                              # 로그인 여부 거짓
@@ -123,12 +130,12 @@ class Login(QWidget, form_class):
                                charset='utf8')
         cursor = conn.cursor()
         # # 데이터 추가하기
-        cursor.execute(f"update beacon set 입실시간 = '{self.time}' where 번호 = {self.result[0][0]}")
+        cursor.execute(f"update beacon set 입실시간 = '{self.time}' where 번호 = {self.result[0][1]}")
         # DB 저장
         conn.commit()
         # DB 닫기
         conn.close()
-        QMessageBox.information(self, "입실", f"{self.result[0][1]}님 입실하셨습니다")
+        QMessageBox.information(self, "입실", f"{self.result[0][2]}님 입실하셨습니다")
         self.enter_time.setText(self.now.strftime('%Y-%m-%d %H:%M'))
 
     def attend_out(self):
@@ -138,12 +145,12 @@ class Login(QWidget, form_class):
                                charset='utf8')
         cursor = conn.cursor()
         # # 데이터 추가하기
-        cursor.execute(f"update beacon set 퇴실시간 = '{self.time}' where 번호 = {self.result[0][0]}")
+        cursor.execute(f"update beacon set 퇴실시간 = '{self.time}' where 번호 = {self.result[0][1]}")
         # DB 저장
         conn.commit()
         # DB 닫기
         conn.close()
-        QMessageBox.information(self, "퇴실", f"{self.result[0][1]}님 퇴실하셨습니다")
+        QMessageBox.information(self, "퇴실", f"{self.result[0][2]}님 퇴실하셨습니다")
         self.finish_time.setText(self.now.strftime('%Y-%m-%d %H:%M'))
 
     def outing(self):
@@ -153,12 +160,12 @@ class Login(QWidget, form_class):
                                charset='utf8')
         cursor = conn.cursor()
         # # 데이터 추가하기
-        cursor.execute(f"update beacon set 외출시간 = '{self.time}' where 번호 = {self.result[0][0]}")
+        cursor.execute(f"update beacon set 외출시간 = '{self.time}' where 번호 = {self.result[0][1]}")
         # DB 저장
         conn.commit()
         # DB 닫기
         conn.close()
-        QMessageBox.information(self, "외출", f"{self.result[0][1]}님 외출하셨습니다")
+        QMessageBox.information(self, "외출", f"{self.result[0][2]}님 외출하셨습니다")
         self.out_time.setText(self.now.strftime('%Y-%m-%d %H:%M'))
 
     def Back(self):
@@ -168,12 +175,12 @@ class Login(QWidget, form_class):
                                charset='utf8')
         cursor = conn.cursor()
         # # 데이터 추가하기
-        cursor.execute(f"update beacon set 복귀시간 = '{self.time}' where 번호 = {self.result[0][0]}")
+        cursor.execute(f"update beacon set 복귀시간 = '{self.time}' where 번호 = {self.result[0][1]}")
         # DB 저장
         conn.commit()
         # DB 닫기
         conn.close()
-        QMessageBox.information(self, "복귀", f"{self.result[0][1]}님 복귀하셨습니다")
+        QMessageBox.information(self, "복귀", f"{self.result[0][2]}님 복귀하셨습니다")
         self.back_time.setText(self.now.strftime('%Y-%m-%d %H:%M'))
 
 
